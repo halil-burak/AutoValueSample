0dedc766b4cfd9185db3e838a66291a6870128a7
halil burak cetinkaya <hburak3534@gmail.com>
2020-12-13 22:22:47 +0300

    lower bounded wildcard example

--- src/main/java/mypkg/TypeInferenceDemo.java
+++ src/main/java/mypkg/TypeInferenceDemo.java
@@ -45,6 +45,34 @@ public class TypeInferenceDemo {
         return sum;
     }
 
+    /**
+     * to print a list of any type, unbounded wildcard example
+     * @param list
+     */
+    public static void printList(List<?> list) {
+        for (Object ele : list) {
+            System.out.print(ele + " ");
+        }
+        System.out.println();
+    }
+
+    /**
+     * a method that puts Integer objects into a list. To maximize flexibility,
+     * you would like the method to work on List<Integer>, List<Number>, and List<Object>
+     *     — anything that can hold Integer values.
+     * To write the method that works on lists of Integer and the supertypes of Integer, such as
+     * Integer, Number, and Object, you would specify List<? super Integer>.
+     * The term List<Integer> is more restrictive than List<? super Integer> because the former matches a list of type Integer only,
+     * whereas the latter matches a list of any type that is a supertype of Integer.
+     * @param list
+     */
+    public static List<Integer> addIntegers(List<? super Integer> list) {
+        for (int i = 0; i < 10; i++) {
+            list.add(i);
+        }
+        return (List<Integer>) list;
+    }
+
     public static void main(String[] args) {
         List<Box<Integer>> integerBoxes = new ArrayList<>();
         TypeInferenceDemo.addBoxes(new Integer(12), integerBoxes);
--- src/test/java/TestTypeInferenceDemo.java
+++ src/test/java/TestTypeInferenceDemo.java
@@ -1,10 +1,13 @@
 import mypkg.TypeInferenceDemo;
 import org.junit.Test;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
+import static mypkg.TypeInferenceDemo.printList;
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 public class TestTypeInferenceDemo {
 
@@ -18,5 +21,22 @@ public class TestTypeInferenceDemo {
 
         assertEquals("Sum is wrong", 6.0, intSum, 0.0);
         assertEquals("Sum is wrong", 13.8, doubleSum, 0.0);
+    }
+
+    @Test
+    public void testGenericPrintList_unboundedWildcard() {
+        List<Integer> li = Arrays.asList(1, 2, 3);
+        List<String>  ls = Arrays.asList("one", "two", "three");
+        printList(li);
+        printList(ls);
+        assertTrue(li.size() == 3);
+        assertTrue(ls.size() == 3);
+    }
+
+    @Test
+    public void testAddNumbers_lowerBoundedWildcardExample() {
+        List<Integer> li = new ArrayList<>();
+        TypeInferenceDemo.addIntegers(li);
+        assertEquals(li.size(), 10);
     }
 }
